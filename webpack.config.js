9b80085ee533318960c91fe9a58d40147f8bd126
PeWe79 <poer.one79@gmail.com>
2024-01-04 02:16:03 +0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,11 @@ const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const isProd = ( process.env.NODE_ENV === 'production' );
 
 // dev server and globals styles
-// const serverHost = '192.168.1.152';
 const serverHost = '0.0.0.0';
 const serverPort = 8080;
 const serverRoot = path.join( __dirname, '/' );
 const appEntry   = './src/app.js';
 const bundleDir  = './public/bundles/';
-
-// get loader config based on env
-// const cssLoaders = () => {
-//   let use = [ 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader' ];
-//   let fallback = isProd ? use.shift() : '';
-//   return isProd ? MiniCssExtractPlugin.extract( { use, fallback } ) : use;
-// }
 
 // webpack config
 module.exports = {
