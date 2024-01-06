d039c4b6b9e724104b072dc467f5c0fc27632129
PeWe79 <poer.one79@gmail.com>
2024-01-06 10:32:32 +0700

    Revert code.

--- src/js/audio.js
+++ src/js/audio.js
@@ -2,95 +2,95 @@
  * Audio handler object
  */
 export default {
-  mContext: null,
-  mAudio: null,
-  mSource: null,
-  mGain: null,
-  mAnalyser: null,
-  mFreq: new Uint8Array(32),
-  mHasfreq: false,
-  mCounter: 0,
-  mEvents: {},
+  _context: null,
+  _audio: null,
+  _source: null,
+  _gain: null,
+  _analyser: null,
+  _freq: new Uint8Array(32),
+  _hasfreq: false,
+  _counter: 0,
+  _events: {},
 
   // setup audio routing, called after user interaction, setup once
   setupAudio() {
-    if (this.mAudio && this.mContext) return;
+    if (this._audio && this._context) return;
 
-    this.mAudio    = new Audio();
-    this.mContext  = new (window.AudioContext || window.webkitAudioContext)();
-    this.mSource   = this.mContext.createMediaElementSource(this.mAudio);
-    this.mAnalyser = this.mContext.createAnalyser();
-    this.mGain     = this.mContext.createGain();
+    this._audio    = new Audio();
+    this._context  = new (window.AudioContext || window.webkitAudioContext)();
+    this._source   = this._context.createMediaElementSource(this._audio);
+    this._analyser = this._context.createAnalyser();
+    this._gain     = this._context.createGain();
 
-    this.mAnalyser.fftSize = 32;
-    this.mSource.connect(this.mAnalyser);
-    this.mSource.connect(this.mGain);
-    this.mGain.connect(this.mContext.destination);
+    this._analyser.fftSize = 32;
+    this._source.connect(this._analyser);
+    this._source.connect(this._gain);
+    this._gain.connect(this._context.destination);
 
-    this.mAudio.addEventListener('canplay', () => {
-      this.mFreq = new Uint8Array(this.mAnalyser.frequencyBinCount);
-      this.mAudio.play();
+    this._audio.addEventListener('canplay', () => {
+      this._freq = new Uint8Array(this._analyser.frequencyBinCount);
+      this._audio.play();
     });
 
     ['waiting', 'playing', 'ended', 'stalled', 'error'].forEach(event => {
-      this.mAudio.addEventListener(event, e => this.emit(event, e));
+      this._audio.addEventListener(event, e => this.emit(event, e));
     });
   },
 
   // add event listeners to the audio api
   on(event, callback) {
     if (event && typeof callback === 'function') {
-      this.mEvents[event] = callback;
+      this._events[event] = callback;
     }
   },
 
   // emit saved audio event
   emit(event, data) {
-    if (event && this.mEvents.hasOwnProperty(event)) {
-      this.mEvents[event](data);
+    if (event && this._events.hasOwnProperty(event)) {
+      this._events[event](data);
     }
   },
 
   // update and return analyser frequency value (0-1) to control animations
   getFreqData(playing) {
-    if (!this.mAnalyser) return 0;
+    if (!this._analyser) return 0;
 
     // this is not working on some devices running safari
-    this.mAnalyser.getByteFrequencyData(this.mFreq);
-    let mFreq = Math.floor(this.mFreq[4] | 0) / 255;
+    this._analyser.getByteFrequencyData(this._freq);
+    let _freq = Math.floor(this._freq[4] | 0) / 255;
 
     // indicate that a freq value can be read
-    if (!this.mHasfreq && mFreq) { this.mHasfreq = true; }
+    if (!this._hasfreq && _freq) { this._hasfreq = true; }
 
     // frequency data available
-    if (this.mHasfreq) return mFreq;
+    if (this._hasfreq) return _freq;
 
     // return fake counter if no freq data available (safari workaround)
     if (playing) {
-      this.mCounter = (this.mCounter < .6) ? (this.mCounter + .01) : this.mCounter;
+      this._counter = (this._counter < .6) ? (this._counter + .01) : this._counter;
     } else {
-      this.mCounter = (this.mCounter > 0) ? (this.mCounter - .01) : this.mCounter;
+      this._counter = (this._counter > 0) ? (this._counter - .01) : this._counter;
     }
-    return this.mCounter;
+    return this._counter;
   },
 
   // set audio volume
   setVolume(volume) {
-    if (!this.mGain) return;
+    if (!this._gain) return;
     volume = parseFloat(volume) || 0;
     volume = (volume > 1) ? (volume / 100) : volume;
     volume = (volume > 1) ? 1 : volume;
     volume = (volume < 0) ? 0 : volume;
-    this.mAudio.muted = (volume <= 0) ? true : false;
-    this.mGain.gain.value = volume;
+    this._audio.muted = (volume <= 0) ? true : false;
+    this._gain.gain.value = volume;
   },
 
   // stop playing audio
   stopAudio() {
-    if (!this.mAudio) return;
-    try { this.mAudio.pause(); } catch (e) {}
-    try { this.mAudio.stop(); } catch (e) {}
-    try { this.mAudio.close(); } catch (e) {}
+    if (!this._audio) return;
+    try { this._audio.pause(); } catch (e) {}
+    try { this._audio.stop(); } catch (e) {}
+    try { this._audio.close(); } catch (e) {}
   },
 
   // play audio source url
@@ -98,16 +98,16 @@ export default {
     this.setupAudio();
     this.stopAudio();
 
-    if (this.mContext.state === 'suspended') {
-      this.mContext.resume().then(() => {
+    if (this._context.state === 'suspended') {
+      this._context.resume().then(() => {
         console.log('Audio context has been resumed.');
       });
     }
-    this.mAudio.src = String(source || '') + '?x=' + Date.now();
-    this.mAudio.preload = 'metadata';
-    this.mAudio.crossOrigin = 'anonymous';
-    this.mAudio.autoplay = false;
-    this.mAudio.load();
+    this._audio.src = String(source || '') + '?x=' + Date.now();
+    this._audio.preload = 'metadata';
+    this._audio.crossOrigin = 'anonymous';
+    this._audio.autoplay = false;
+    this._audio.load();
   },
 
 }
--- src/js/scene.js
+++ src/js/scene.js
@@ -4,38 +4,38 @@
 import Sphere from './sphere';
 
 export default {
-  mWrap: null,
-  mCanvas: null,
-  mRenderer: null,
-  mScene: null,
-  mCamera: null,
-  mBox: null,
-  mMouse: { x: 0, y: 0 },
-  mObjects: [],
+  _wrap: null,
+  _canvas: null,
+  _renderer: null,
+  _scene: null,
+  _camera: null,
+  _box: null,
+  _mouse: { x: 0, y: 0 },
+  _objects: [],
 
   // setup animation canvas
   setupCanvas() {
-    this.mWrap   = document.querySelector('#player-wrap');
-    this.mCanvas = document.querySelector('#player-canvas');
-    this.mBox    = this.mWrap.getBoundingClientRect();
+    this._wrap   = document.querySelector('#player-wrap');
+    this._canvas = document.querySelector('#player-canvas');
+    this._box    = this._wrap.getBoundingClientRect();
 
     // setup scene and renderer
-    this.mScene = new THREE.Scene();
-    this.mRenderer = new THREE.WebGLRenderer({ canvas: this.mCanvas, alpha: true, antialias: true, precision: 'lowp' });
-    this.mRenderer.setClearColor(0x000000, 0);
-    this.mRenderer.setPixelRatio(window.devicePixelRatio);
+    this._scene = new THREE.Scene();
+    this._renderer = new THREE.WebGLRenderer({ canvas: this._canvas, alpha: true, antialias: true, precision: 'lowp' });
+    this._renderer.setClearColor(0x000000, 0);
+    this._renderer.setPixelRatio(window.devicePixelRatio);
 
     // setup camera
-    this.mCamera = new THREE.PerspectiveCamera(60, (this.mBox.width / this.mBox.height), 0.1, 20000);
-    this.mCamera.lookAt(this.mScene.position);
-    this.mCamera.position.set(0, 0, 300);
-    this.mCamera.rotation.set(0, 0, 0);
+    this._camera = new THREE.PerspectiveCamera(60, (this._box.width / this._box.height), 0.1, 20000);
+    this._camera.lookAt(this._scene.position);
+    this._camera.position.set(0, 0, 300);
+    this._camera.rotation.set(0, 0, 0);
 
     // add and create objects
-    this.mObjects.push(Sphere);
+    this._objects.push(Sphere);
 
-    for (let o of this.mObjects) {
-      o.create(this.mBox, this.mScene);
+    for (let o of this._objects) {
+      o.create(this._box, this._scene);
     }
     // setup events
     window.addEventListener('mousemove', this.updateMouse.bind(this));
@@ -46,35 +46,35 @@ export default {
 
   // update custom objects in 3d scene
   updateObjects(freq) {
-    for (let o of this.mObjects) {
-      o.update(this.mBox, this.mMouse, freq);
+    for (let o of this._objects) {
+      o.update(this._box, this._mouse, freq);
     }
-    this.mRenderer.render(this.mScene, this.mCamera);
+    this._renderer.render(this._scene, this._camera);
   },
 
   // update canvas size
   updateSize() {
-    if (!this.mWrap || !this.mCanvas) return;
-    this.mBox = this.mWrap.getBoundingClientRect();
-    this.mCanvas.width = this.mBox.width;
-    this.mCanvas.height = this.mBox.height;
-    this.mCamera.aspect = (this.mBox.width / this.mBox.height);
-    this.mCamera.updateProjectionMatrix();
-    this.mRenderer.setSize(this.mBox.width, this.mBox.height);
+    if (!this._wrap || !this._canvas) return;
+    this._box = this._wrap.getBoundingClientRect();
+    this._canvas.width = this._box.width;
+    this._canvas.height = this._box.height;
+    this._camera.aspect = (this._box.width / this._box.height);
+    this._camera.updateProjectionMatrix();
+    this._renderer.setSize(this._box.width, this._box.height);
   },
 
   // update mouse position from center of canvas
   updateMouse(e) {
-    if (!this.mBox) return;
-    const centerX = this.mBox.left + (this.mBox.width / 2);
-    const centerY = this.mBox.top + (this.mBox.height / 2);
+    if (!this._box) return;
+    const centerX = this._box.left + (this._box.width / 2);
+    const centerY = this._box.top + (this._box.height / 2);
 
     if (e) {
-      this.mMouse.x = Math.max(0, e.pageX || e.clientX || 0) - centerX;
-      this.mMouse.y = Math.max(0, e.pageY || e.clientY || 0) - centerY;
+      this._mouse.x = Math.max(0, e.pageX || e.clientX || 0) - centerX;
+      this._mouse.y = Math.max(0, e.pageY || e.clientY || 0) - centerY;
     } else {
-      this.mMouse.x = centerX;
-      this.mMouse.y = centerY;
+      this._mouse.x = centerX;
+      this._mouse.y = centerY;
     }
   },
 }
