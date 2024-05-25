f2bd7bd8b9580490fd4d8f82c0b8d6761efe5fa3
PeWe79 <poer.one79@gmail.com>
2024-05-25 23:57:46 +0700

    Update README.md.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[site]: https://cloudmu.id/
+[site]: https://github.com/PeWe79/
 [mit]: https://www.opensource.org/licenses/mit-license.php
 [repo]: https://github.com/PeWe79/AzuraCast-Player/
 [azuracast]: https://demo.azuracast.com/
@@ -12,20 +12,52 @@ This is a fully modified version of SomaFM Music Player from [rainner/soma-fm-pl
 This Web project will be grab all radio station from AzuraCast streaming server via API interface.
 
 ## Demo Screenshots
-
 ![AzuraCast-Player](https://github.com/PeWe79/AzuraCast-Player/blob/main/thumb.png)
 
 ![AzuraCast-Player](https://github.com/PeWe79/AzuraCast-Player/blob/main/thumb2.png)
 
 This is a Vue.js web application for streaming radio stations from [demo.azuracast.com][azuracast]. This app uses the public AzuraCast JSON channels API endpoint to pull in a list of stations and makes it easy to switch between stations. This app also used Three.js and the [HTML5 Web Audio Context API][audioapi] to sample audio data and create a visualizer effect for the selected station.
 
+## Documentation.
+Open the [config.js](https://github.com/PeWe79/AzuraCast-Player/blob/main/src/js/config.js) file and edit the line below :
+
+```javascript
+apiBaseUrl: 'https://your-azuracast-server.com'
+
+```
+
+## Install
+Clone the repository [AzuraCast-Player][repo] then :
+
+### Project setup
+```
+npm install
+```
+### Compiles for development
+```
+npm run dev
+```
+
+### Compiles and hot-reloads for development
+```
+npm run init
+```
+### Compiles and minifies for production
+```
+npm run build
+```
+
+## Supported Cover Artworks
+* iTunes
+
+## TODO
+* Option get Cover Artworks from Deezer or/and from Spotify.
+* Make song request form and work with AzuraCast server.
 
 ### Author
-
 [PeWe79][site]
 
 ### License
-
 Licensed under [MIT][mit].
 
 ## Credits
--- src/js/api.js
+++ src/js/api.js
@@ -8,47 +8,47 @@ export default {
 
   // get station data from api
   getChannels(callback) {
-    const apiurl = config.apiBaseUrl+'/api/stations';
-    const error  = 'There was a problem fetching the latest list of music channels from AzuraCast.';
+    const apiurl = config.apiBaseUrl + '/api/stations';
+    const error = 'There was a problem fetching the latest list of music channels from AzuraCast.';
 
     axios.get(apiurl).then(res => {
       const list = this._parseChannels(res.data);
       if (!list.length) return callback(error, []);
       return callback(null, list);
     })
-    .catch(e => {
-      return callback(error + String(e.message || ''), []);
-    });
+      .catch(e => {
+        return callback(error + String(e.message || ''), []);
+      });
   },
 
   // fetch songs for a channel
   getSongs(channel, callback) {
     const apiurl = channel.songsurl || '';
-    const title  = channel.name || '...';
-    const error  = 'There was a problem loading the list of songs for channel '+ title +' from AzuraCast.';
+    const title = channel.name || '...';
+    const error = 'There was a problem loading the list of songs for channel ' + title + ' from AzuraCast.';
 
     axios.get(apiurl).then(res => {
       if (!res.data) return callback(error, []);
       return callback(null, res.data);
     })
-    .catch(e => {
-      return callback(error + String(e.message || ''), []);
-    });
+      .catch(e => {
+        return callback(error + String(e.message || ''), []);
+      });
   },
 
   // fetch next song for a channel
   getNextSong(channel, callback) {
     const apiurl = channel.songsurl || '';
-    const title  = channel.name || '...';
-    const error  = 'There was a problem loading the next song for channel '+ title +' from AzuraCast.';
+    const title = channel.name || '...';
+    const error = 'There was a problem loading the next song for channel ' + title + ' from AzuraCast.';
 
     axios.get(apiurl).then(res => {
       if (!res.data) return callback(error, []);
       return callback(null, res.data);
     })
-   .catch(e => {
-      return callback(error + String(e.message || ''), []);
-    });
+      .catch(e => {
+        return callback(error + String(e.message || ''), []);
+      });
   },
 
   // fetch next song for a channel
@@ -58,17 +58,17 @@ export default {
     let output = [];
     if (Array.isArray(station)) {
       for (let c of station) {
-        c.plsfile   = c.playlist_pls_url;
-        c.mp3file   = c.listen_url;
-        c.songsurl  = config.apiBaseUrl + '/api/nowplaying/' + c.id;
-        c.image     = '/public/img/'+c.shortcode+'.png';
-        c.infourl   = c.url;
-        c.twitter   = c.twitter ? 'https://twitter.com/@'+ c.twitter : '';
-        c.route     = '/station/'+ c.shortcode;
+        c.plsfile = c.playlist_pls_url;
+        c.mp3file = c.listen_url;
+        c.songsurl = config.apiBaseUrl + '/api/nowplaying/' + c.id;
+        c.image = '/public/img/' + c.shortcode + '.png';
+        c.infourl = c.url;
+        c.twitter = c.twitter ? 'https://twitter.com/@' + c.twitter : '';
+        c.route = '/station/' + c.shortcode;
         c.listeners = c.mounts[0].listeners.current | 0;
-        c.updated   = c.updated | 0;
-        c.favorite  = false;
-        c.active    = false;
+        c.updated = c.updated | 0;
+        c.favorite = false;
+        c.active = false;
         output.push(c);
         // console.log("DATA => "+c.image);
       }
